85b3dae23e5ade039d978d1ed6e60b6d80a02233
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-08-20 12:17:08 +0300

    Change Test

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.text;
 
 
 
@@ -13,11 +14,10 @@ public class CardOrderTest {
     @Test
     void shouldEnteringValidValues() {
         open("http://localhost:9999");
-        SelenideElement form =$("[data-test-id=callback-form]");
-        form.$("[data-test-id=name] input").setValue("Борис Бритва");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
-        form.$("[data-test-id=agreement]").click();
-        form.$("[type='button']").click();
+        $("[data-test-id=name] input").setValue("Борис Бритва");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
