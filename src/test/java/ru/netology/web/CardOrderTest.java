66131001cff9d2055ddc65f1260bd062bf6f49e8
DarcenRal <75911924+DarcenRal@users.noreply.github.com>
2021-08-20 12:56:53 +0300

    Add new tests

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -20,4 +20,44 @@ public class CardOrderTest {
         $("[type='button']").click();
         $("[data-test-id='order-success']").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
+
+    @Test
+    void shouldWithoutPhoneNumber() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Борис Бритва");
+        $("[data-test-id=phone] input").setValue("");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldWithoutName() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='name']").shouldHave(text("Поле обязательно для заполнения"));
+    }
+
+    @Test
+    void shouldEmptyCheckBox() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Борис Бритва");
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        //$("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='agreement']").shouldHave(text("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    @Test
+    void shouldNotValidPhoneNumber() {
+        open("http://localhost:9999");
+        $("[data-test-id=name] input").setValue("Борис");
+        $("[data-test-id=phone] input").setValue("89270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type='button']").click();
+        $(".input_invalid[data-test-id='phone']").shouldHave(text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+    }
 }
